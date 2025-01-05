bb1b33b9f65b93c1ee1852c24a41f6414c9bc6d2
abdessalam-ctrl <abdessalam.maiza@centrale-casablanca.ma>
2025-01-05 18:01:53 +0100

    Update test_main_game.py

--- code/tests/test_main_game.py
+++ code/tests/test_main_game.py
@@ -9,10 +9,10 @@ def test_main_game_execution():
     try:
         # Si la fonction principale de votre jeu se trouve dans main_game.py,
         # assurez-vous de l'importer ici. Par exemple :
-        import main_game  # Remplacez par le nom réel de votre fichier
+        import main-game  # Remplacez par le nom réel de votre fichier
 
         # Appelons la fonction main qui lance le jeu
-        main_game.main()  # Remplacez par la fonction qui démarre le jeu
+        main-game.main()  # Remplacez par la fonction qui démarre le jeu
     except Exception as e:
         # Si une erreur se produit, le test échoue
         pytest.fail(f"Le jeu a échoué à s'exécuter : {e}")
